cf63a28e5e489bcb9705cdd1891ee01727207531
Sushma Karpe <68609047+sushmadakh@users.noreply.github.com>
2022-06-10 16:14:54 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
 // import using require
-
+var Shape = require('./shape');
 // declare class
-
+class Circle extends Shape{
+    constructor(){
+     super();   
+    }
+    calculateArea(){
+        
+    }
+}
 
 // export class using module.exports
+module.exports = Circle;
