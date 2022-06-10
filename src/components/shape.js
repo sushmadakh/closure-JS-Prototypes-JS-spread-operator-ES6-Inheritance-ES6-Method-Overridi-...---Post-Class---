5757c770f43a21d7c38185d96915cf71e3f8b802
Sushma Karpe <68609047+sushmadakh@users.noreply.github.com>
2022-06-10 16:15:51 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
+class Shape{
+    constructor(color){
+        this.color = {color:'red'};
+    }
+    drawShape(){
+
+    }
+    calculateArea(){
+        
+    }
+}
+
+
 // export class using module.exports
+module.exports = Shape;
